d7cb8945006185d4da7354be225d45eff4118450
ZachlitMoon <66971407+zacharykmoon@users.noreply.github.com>
2020-06-17 15:15:28 -0700

    Update SpeakAndSpell.java

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -11,10 +11,13 @@ public class SpeakAndSpell {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
 			JOptionPane.showMessageDialog(null, "spell mandlebrot" );
 		// 2. Catch the user's answer in a String
-			String 
+			String answer = 
 
 		// 3. If the user spelled the word correctly, speak "correct"
-
+			if (answer == "mandlebrot")
+				speak("correct");
+			else
+				speak("wrong");
 		// 4. Otherwise say "wrong"
 
 		// 5. repeat the process for other words
