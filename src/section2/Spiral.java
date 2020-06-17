aae96389f83b2cca6c7a70f6709cff8b5a7b2e9e
ZachlitMoon <66971407+zacharykmoon@users.noreply.github.com>
2020-06-17 02:40:38 +0000

    Update Spiral.java

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -1,25 +1,34 @@
 package section2;
 
+import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot rob = new Robot();
 		// 5. Set your robot's pen down 
-		
+		rob.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		int speed = 100;
+		rob.setSpeed(speed);
+		int turnRadius = 360/7;
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for (int i=0; i<75;i++) {
 			// 7. Change the pen color to random
-		
-			// 6. Move the robot 5 times the loop counter (5*i)
+			rob.setRandomPenColor();
+				// 6. Move the robot 5 times the loop counter (5*i)
+			rob.move(5*i);
+				
+				// 2. Turn the robot 360/7 degrees to the right
 			
-			// 2. Turn the robot 360/7 degrees to the right
+			rob.turn(turnRadius);
+				// 8. Set the pen width to i
+			rob.setPenWidth(i);
+		}
 		
-			// 8. Set the pen width to i
+			
 			
 	}
 }
