4f4f05743e4dc8176bbdf2d49f08e17168dcd1d5
root <root@622c5b382c7f>
2020-06-18 19:30:41 +0000

    Stephen Hawking

--- src/section3/StephenHawking.java
+++ src/section3/StephenHawking.java
@@ -7,20 +7,20 @@ import com.sun.speech.freetts.VoiceManager;
 public class StephenHawking {
 	
 	// 1. make a main method and put steps 2, 3 & 4 inside it
-	
+	public static void main(String[] args) {
 	// 4. Use a for loop to repeat steps #2 and #3, a lot of times
-	
+		for (int i=0; i<10; i++) {
 	// 2. ask the user for a sentence
-	
+			String input = JOptionPane.showInputDialog("Type in a sentence");
+			
 	// 3. call the speak method below and send it the sentence
+			speak(input);
+		}
 
 	
-
 	
 	
-	
-	
-	
+	}
 	
 	
 	/* Don't change this. */
